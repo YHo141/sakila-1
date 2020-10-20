745ff9739a0e10ac5e9bdac2a39b0a228aed805b
bur56 <bur56@DESKTOP-1NG4I6B>
2020-10-20 11:21:57 +0900

    Class.forName()호출 listener

---
+++ sakila/src/sakila/listener/DBListener.java
@@ -0,0 +1,23 @@
+package sakila.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class DBListener implements ServletContextListener {
+    public DBListener() {}
+    public void contextDestroyed(ServletContextEvent sce)  { }
+
+    public void contextInitialized(ServletContextEvent sce)  { 
+        System.out.println("DBListener.contextInitialized() 실행");
+        try {
+        	Class.forName("");
+        	System.out.println("mariadb 드라이브로딩 성공");
+		} catch (Exception e) {
+			System.out.println("Class.forName() 실행 실패 !");
+			e.printStackTrace();
+		}
+    }
+	
+}
